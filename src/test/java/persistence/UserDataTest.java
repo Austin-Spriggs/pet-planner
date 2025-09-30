e6e6913c721c923135fc7b2aaaf4aae267b27005
Austin Spriggs <aspriggs@madisoncollege.edu>
2025-09-30 01:46:57 -0500

    added rest of tests / jdoc comments / modified cleanDB sql

--- src/test/java/persistence/UserDataTest.java
+++ src/test/java/persistence/UserDataTest.java
@@ -6,55 +6,113 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class for testing UserData methods
+ *
+ */
 class UserDataTest {
 
-    UserData userData;
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    UserData userData;
 
+    /**
+     *  Test setup
+     *
+     */
     @BeforeEach
     void setUp() {
+        userData = new UserData();
+
         Database database = Database.getInstance();
         database.runSQL("cleanDB.sql");
 
     }
 
+    /**
+     * Get by id test
+     *
+     */
     @Test
     void getById() {
-        UserData userData = new UserData();
         User retrievedUser = userData.getById(1);
 
-        assertEquals("Test", retrievedUser.getFirstName());
+        assertEquals("Austin", retrievedUser.getFirstName());
     }
 
+    /**
+     * Update data test
+     *
+     */
     @Test
     void update() {
+        User userToUpdate = userData.getById(1);
+        userToUpdate.setFirstName("Poopyhead");
+        userData.update(userToUpdate);
+
+        User updatedUser = userData.getById(1);
+        assertEquals("Poopyhead", updatedUser.getFirstName());
     }
 
+    /**
+     * Insert data test
+     *
+     */
     @Test
     void insert() {
-        UserData userData = new UserData();
-        User userToInsert = new User ("Test", "Test", "test", "test", "test@email.com");
+        User userToInsert = new User ("testInsert", "testInsert", "testInsert", "testInsert", "testInsert@email.com");
         int insertedUserId = userData.insert(userToInsert);
 
         assertTrue(insertedUserId != 0);
     }
 
+    /**
+     * Delete data test
+     *
+     */
     @Test
     void delete() {
+        User userToDelete = userData.getById(1);
+        userData.delete(userToDelete);
+
+        User retrievedUser = userData.getById(1);
+        assertNull(retrievedUser);
     }
 
+    /**
+     * Get all data test
+     *
+     */
     @Test
     void getAll() {
+        List<User> retrievedUsers = userData.getAll();
+
+        assertFalse(retrievedUsers.isEmpty());
     }
 
+    /**
+     * Get property equal to test
+     *
+     */
     @Test
     void getByPropertyEqual() {
+        List<User> retrievedUsers = userData.getByPropertyEqual("lastName", "Spriggs");
+
+        assertFalse(retrievedUsers.isEmpty());
     }
 
+    /**
+     * Get property like test
+     *
+     */
     @Test
     void getByPropertyLike() {
+        List<User> retrievedUsers = userData.getByPropertyLike("lastName", "Spriggs");
+
+        assertFalse(retrievedUsers.isEmpty());
     }
 }
--- src/test/resources/cleanDB.sql
+++ src/test/resources/cleanDB.sql
@@ -1,12 +1,18 @@
-drop table if exists 'users';
-create table users
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
 (
-    id        int auto_increment
-        primary key,
+    id        int auto_increment primary key,
     firstName varchar(15)  not null,
     lastName  varchar(20)  not null,
     email     varchar(50)  not null,
     username  varchar(15)  not null,
     password  varchar(255) not null
 );
+
+INSERT INTO users
+    (firstName, lastName, email, username, password)
+VALUES
+    ('Austin', 'Spriggs', 'aspriggs@email.com', 'aspriggs', 'aspriggs')
+;
 

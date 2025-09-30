9916c80c0f4aa7e091da945dcb4eb2a6bf881e92
Austin Spriggs <aspriggs@madisoncollege.edu>
2025-09-30 01:55:06 -0500

    adde jdoc comments

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -26,10 +26,23 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    /**
+     * Default User constructor
+     *
+     */
     public User() {
 
     }
 
+    /**
+     * User constructor that sets all variables
+     *
+     * @param firstName - user's firstName
+     * @param lastName - user's lastName
+     * @param username - user's username
+     * @param password - user's password
+     * @param email - user's email
+     */
     public User(String firstName, String lastName, String username, String password, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -38,51 +51,110 @@ public class User {
         this.email = email;
     }
 
-
+    /**
+     * Getter for id
+     *
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for id
+     *
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for firstName
+     *
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Setter for firstName
+     *
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Getter for lastName
+     *
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Setter for lastName
+     *
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Getter for username
+     *
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     *
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for password
+     *
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Getter for email
+     *
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter for email
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
